381e3448135266598eccb680df67a5d536ad6467
Elyas <Elyas.shamal2@gmail.com>
2023-11-30 23:06:16 -0500

    add math-object

---
+++ 01-variables-data-types/10-Math-object/main.js
@@ -0,0 +1,4 @@
+// Math Object
+
+// In JavaScript, the Math object is a built-in object that provides mathematical functions and constants.
+// It is not a constructor like some other objects in JavaScript, such as Array or Date.
